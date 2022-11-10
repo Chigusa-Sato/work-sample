8c37cc3cf54c858760ed0cbf595f005fe03eec0a
Chigusa-Sato <chigusa.sato@rakus-partners.co.jp>
2022-11-10 23:30:42 +0900

    '一覧画面の仮レイアウト実装'

--- src/pages/Images.vue
+++ src/pages/Images.vue
@@ -1,3 +1,72 @@
 <template>
-    images.vue
+  <div class="card_list">
+    <div class="card_item">
+      <img
+        class="card_item_img"
+        src="../assets/anita-austvika-W2M7vaZSl0w-unsplash.jpg"
+        alt=""
+      />
+    </div>
+    <div class="card_item">
+      <img
+        class="card_item_img"
+        src="../assets/anita-austvika-W2M7vaZSl0w-unsplash.jpg"
+        alt=""
+      />
+    </div>
+    <div class="card_item">
+      <img
+        class="card_item_img"
+        src="../assets/anita-austvika-W2M7vaZSl0w-unsplash.jpg"
+        alt=""
+      />
+    </div>
+    <div class="card_item">
+      <img
+        class="card_item_img"
+        src="../assets/anita-austvika-W2M7vaZSl0w-unsplash.jpg"
+        alt=""
+      />
+    </div>
+  </div>
 </template>
+
+<script>
+import { defineComponent, reactive } from 'vue';
+
+const getUniqueStr = (myStrong) => {
+  let strong = 1000;
+  if (myStrong) strong = myStrong;
+  return (
+    new Date().getTime().toString(16) +
+    Math.floor(strong * Math.random()).toString(16)
+  );
+};
+
+export default defineComponent({
+  setup() {
+    const imageList = reactive(new Array());
+    const imageItem = { id: getUniqueStr(), img: 'hahahah' };
+
+    return { imageItem };
+  },
+});
+</script>
+
+<style>
+.card_list {
+  display: flex;
+  justify-content: space-around;
+}
+
+.card_item {
+  width: 250px;
+  height: 250px;
+  border: 1px solid #a0a0a0;
+  display: flex;
+  align-items: center;
+}
+.card_item_img {
+  width: 100%;
+}
+</style>
