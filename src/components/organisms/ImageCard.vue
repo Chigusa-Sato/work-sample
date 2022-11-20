c40ce39c4c8fe95cee7c4d397c5352f522bc08c4
Chigusa-Sato <chigusa.sato@rakus-partners.co.jp>
2022-11-20 18:56:28 +0900

    'カードコンポーネントを追加'

---
+++ src/components/organisms/ImageCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="card">
+    <img :src="imageItem.url" alt="" />
+    <button class="button__edit">編集</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent, reactive } from 'vue';
+export default defineComponent({
+  props: {
+    imageItem: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props, { emit }) {
+    props = reactive(props);
+
+    const onClickEvent = () => {
+      emit('clickEvent');
+    };
+    return { onClickEvent };
+  },
+});
+</script>
+
+<style scoped>
+.card {
+  width: 250px;
+  height: 250px;
+  border: 1px solid #a0a0a0;
+  padding: 5px;
+  flex-shrink: 0;
+  position: relative;
+}
+.card img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+.button__edit {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  /* MEMO:ここでcssの上書はＮＧかもしれないので確認 */
+  width: 50px;
+  background: black;
+  color: white;
+  border: 2px solid black;
+  opacity: 60%;
+}
+
+.button__edit:hover {
+  background: white;
+  border: 2px solid black;
+  color: black;
+}
+</style>
